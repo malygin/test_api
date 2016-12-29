7a3154e32f343f1185bf4ba34dcfab2ec7bcbbc9
Andrey Malygin <anmalygin@gmail.com>
2016-12-29 21:30:27 +0300

    fix tests

--- test/integration/url_list_test.rb
+++ test/integration/url_list_test.rb
@@ -1,13 +1,12 @@
 require 'test_helper'
-class UrlLoadTest < ActionDispatch::IntegrationTest
+class UrlListTest < ActionDispatch::IntegrationTest
   setup do
-    @one = stored_contents(:one)
+    stored_contents(:one)
     stored_contents(:two)
   end
 
   test 'list content'    do
     get '/api/list_content'
-    puts StoredContent.all.size, response.body
     assert_equal 200, response.status
     assert_includes response.body, 'h1Content'
     assert_includes response.body, 'ahref'
